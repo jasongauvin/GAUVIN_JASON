f8416e3355e1a9aa59bfd33a5a229cbbff7dbcef
jasongauvin <contact.jason.gauvin@gmail.com>
2020-12-16 17:18:07 +0100

    feat: Add other struc to migration function

--- api/database/migrations.go
+++ api/database/migrations.go
@@ -4,6 +4,7 @@ import (
 	"github.com/jasongauvin/GAUVIN_JASON/api/models"
 )
 
+// MakeMigrations load sata structure on db
 func MakeMigrations() {
-	DB.AutoMigrate(&models.Customer{}, &models.Form{})
+	DB.AutoMigrate(&models.Customer{}, &models.Form{}, &models.Question{}, &models.MultiChoice{}, &models.Slider{})
 }
