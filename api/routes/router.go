2b82b48b8673cd0a07246d95476ea9a64c1871fa
jasongauvin <contact.jason.gauvin@gmail.com>
2020-12-16 14:09:10 +0100

    feat: Add JWT and middleware authentication

--- api/routes/router.go
+++ api/routes/router.go
@@ -1,19 +1,25 @@
 package routes
 
 import (
+	"github.com/jasongauvin/GAUVIN_JASON/api/middleware"
 	"github.com/gin-gonic/gin"
-	"github.com/jasongauvin/gogoleplate/api/controllers"
+	"github.com/jasongauvin/GAUVIN_JASON/api/controllers"
 )
 
 // InitializeRoutes set up the routes for the server
 func InitializeRoutes(r *gin.Engine) {
 	// HTML routes for the GUI
 	r.GET("/", controllers.SayHello)
+	r.POST("/register", controllers.Register)
+	r.POST("/login", controllers.Login)
 
 	// API routes
 	api := r.Group("/api")
+	// Check Authorization for api grouip
+	api.Use(middleware.CheckAuthorization)
+
 	{
-		api.GET("/register", controllers.Register)
-		api.GET("/login", controllers.Login)
+		api.POST("/form", controllers.CreateForm)
+
 	}
 }
